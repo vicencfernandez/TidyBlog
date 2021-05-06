5aa1a7738d77203738b83660e71b3241ee89372d
Vicenc Fernandez <vicenc.fernandez@gmail.com>
2021-05-06 17:35:35 +0200

    re-install

--- helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'excerpt' => function ($page, $limit = 250, $end = '...') {
-        return $page->isPost
-            ? str_limit_soft(content_sanitize($page->getContent()), $limit, $end)
-            : null;
-    },
-];
